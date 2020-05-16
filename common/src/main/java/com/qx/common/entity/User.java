56225712a6fd3b32efce7ab079511860e94b36e3
shusheng <2430239894@qq.com>
2020-05-16 20:43:44 +0800

    springcloud

---
+++ common/src/main/java/com/qx/common/entity/User.java
@@ -0,0 +1,43 @@
+package com.qx.common.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private String userName;
+    private String password;
+    private String email;
+    private Date birthday;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
